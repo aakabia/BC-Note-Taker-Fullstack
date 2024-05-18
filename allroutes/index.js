60e02873a2816850f5d1c7b0bb8728bf5e16cbd7
Aziz Kabia <158849915+aakabia@users.noreply.github.com>
2024-05-17 21:46:27 -0400

    post request done

--- allroutes/index.js
+++ allroutes/index.js
@@ -3,10 +3,13 @@ const router = require('express').Router();
 // Above,, I must make sure to use the router version of express here.
 
 const notesRouter = require('./getnotes');
+const postRouter = require('./postnotes');
+
 
 // Above, I import the routes from getnotes
 
 router.use('/notes', notesRouter);
+router.use('/notes', postRouter );
 
 // Above, I assign these routes a path 
 
